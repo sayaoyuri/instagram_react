25dee93381b915bae0accb650c541404ba9ae39d
sayaoyuri <sayaoyuri.dev@gmail.com>
2023-05-04 15:13:24 -0300

    Like

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,4 +1,27 @@
+import { useState } from "react";
+
 export default function Post (post) {
+  const [like, setLike] = useState('heart-outline');
+  const [bookmarked, setBookmark] = useState('bookmark-outline')
+
+  function likePost(ev) {
+    ev.preventDefault();
+
+    like === 'heart-outline' ? setLike('heart') : setLike('heart-outline');
+  }
+
+  function likePostDBClick (ev) {
+    ev.preventDefault();
+
+    setLike('heart');
+  }
+
+  function savePost(ev) {
+    ev.preventDefault();
+
+    bookmarked === 'bookmark-outline' ? setBookmark('bookmark') : setBookmark('bookmark-outline');
+  }
+
   return (
     <div class="post">
       <header>
@@ -8,14 +31,14 @@ export default function Post (post) {
           </div>
         <ion-icon name="ellipsis-horizontal-outline"></ion-icon>
       </header>
-        <img src={post.image} alt="" />
+        <img src={post.image} onDoubleClick={likePostDBClick} alt="" />
         <div class="interactions">
         <div>
-        <ion-icon name="heart-outline"></ion-icon>
+        <ion-icon name={like} onClick={likePost}></ion-icon>
         <ion-icon name="chatbubble-outline"></ion-icon>
           <ion-icon name="paper-plane-outline"></ion-icon>
           </div>
-        <ion-icon name="bookmark-outline"></ion-icon>
+        <ion-icon name={bookmarked} onClick={savePost}></ion-icon>
         </div>
       </div>
   );
